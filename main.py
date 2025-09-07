cd5aa56f331c466da29e3765957621a514ec92e7
filipepaganelli <paganelli.filipe@gmail.com>
2025-09-07 19:05:55 -0300

    Applies POO into the code

--- main.py
+++ main.py
@@ -3,132 +3,126 @@ import time
 import sys
 import tkinter as tk
 
-#Iniiating the root window and setting attributes 
-root = tk.Tk()
-root.title("Calculator-Paganelli")
-root.geometry("300x400")
-root.resizable(False, False)
-root.grid_columnconfigure(0,weight=1)
-root.grid_rowconfigure(2,weight=1)
+
+class CalculatorInterface:
+    def __init__(self):     
+        #Iniiating the root window and setting attributes 
+        self.root = tk.Tk()
+        self.root.title("Calculator-Paganelli")
+        self.root.geometry("300x400")
+        self.root.resizable(False, False)
+
+        self.buttons_font = ("Arial", 15) 
+
+        self.create_display()
+        self.create_buttons_frame()
+        self.configure_layout()
+        self.bind_keys()
+        self.create_buttons()
+
+
+    def create_display(self):
+        #Display settings
+        self.display_var = tk.StringVar()
+        self.display = tk.Entry(
+            self.root, 
+            textvariable=self.display_var, 
+            borderwidth=5, 
+            justify="center", 
+            font=("Arial", 24), 
+            state="readonly")
+
+        self.display.grid(rowspan=2,columnspan=4,sticky="ew", padx=0, pady=5)
+
+    def create_buttons_frame(self):
+        #Buttons Frame
+        self.buttons_frame = tk.Frame(self.root)
+        self.buttons_frame.grid(row=2, column=0, columnspan=5,sticky="nsew", padx=5, pady=5) 
+
+        
+
+        #Attributing weights to the rows and column so the behave dynamic 
+        for i in range(4):
+            self.buttons_frame.grid_columnconfigure(i, weight=1)
+
+        for i in range(7):
+            self.buttons_frame.grid_rowconfigure(i, weight=1)
+
+    def configure_layout(self): 
+        self.root.grid_columnconfigure(0,weight=1)
+        self.root.grid_rowconfigure(2,weight=1)
+
+    def bind_keys(self):    
+        self.root.bind("<Key>", self.handle_keypress)
+
+    def handle_keypress(self,event):
+        if event.char in "0123456789+-*/":
+            self.insert_number_on_display(event.char)
+        elif event.keysym=="Return":
+            self.calculate_result()
+        elif event.keysym=="BackSpace":
+            self.erase_values()
+
+    def run(self):
+        self.root.mainloop()
+
+
+    def insert_number_on_display(self,value):
+        self.display.configure(state="normal")
+        self.display_var.set(self.display_var.get() + str(value))
+        self.display.configure(state="readonly")
+
+    def clean_display(self):
+        self.display.configure(state="normal")
+        self.display_var.set("")
+        self.display.configure(state='readonly')# Doesn't let the user edit the values directly on the display
+
+    def calculate_result(self):
+        try:
+            result= eval(self.display_var.get())
+            result = int(result) if isinstance(result, float) and result.is_integer() else result #As the result of eval() always return a float, this code checks if it's integer to format the display value with no d
+            result= str(result)
+            self.display_var.set(result)
+        
+        except:
+            self.display.configure(state="normal")
+            self.display_var.set("Error!")
+            self.display.configure(state="readonly")
+
+    def erase_values(self):
+        display_values = self.display_var.get()
+        self.display_var.set(display_values[:-1]) #Erases the last characterer inserted
 
 
 
-#Display settings
-display_var = tk.StringVar()
-display = tk.Entry(root, textvariable=display_var, borderwidth=5, justify="center", font=("Arial", 24), state="readonly")
-display.grid(rowspan=2,columnspan=4,sticky="ew", padx=0, pady=5)
+    def create_buttons(self):
+        buttons = [
+            ("C",2,0,4, self.clean_display),
+            ("7", 3, 0, 1, lambda: self.insert_number_on_display("7")),
+            ("8", 3, 1, 1, lambda: self.insert_number_on_display("8")),
+            ("9", 3, 2, 1, lambda: self.insert_number_on_display("9")),
+            ("/", 3, 3, 1, lambda: self.insert_number_on_display("/")),
+            ("4", 4, 0, 1, lambda: self.insert_number_on_display("4")),
+            ("5", 4, 1, 1, lambda: self.insert_number_on_display("5")),
+            ("6", 4, 2, 1, lambda: self.insert_number_on_display("6")),
+            ("x", 4, 3, 1, lambda: self.insert_number_on_display("*")),
+            ("1", 5, 0, 1, lambda: self.insert_number_on_display("1")),
+            ("2", 5, 1, 1, lambda: self.insert_number_on_display("2")),
+            ("3", 5, 2, 1, lambda: self.insert_number_on_display("3")),
+            ("-", 5, 3, 1, lambda: self.insert_number_on_display("-")),
+            ("0", 6, 0, 1, lambda: self.insert_number_on_display("0")),
+            (".", 6, 1, 1, lambda: self.insert_number_on_display(".")),
+            ("=", 6, 2, 1, self.calculate_result),
+            ("+", 6, 3, 1, lambda: self.insert_number_on_display("+")),
+        ]
 
-buttons_frame = tk.Frame(root)
-buttons_frame.grid(row=2, column=0, columnspan=5,sticky="nsew", padx=5, pady=5) 
+        for (text, row, column, columnspan, command) in buttons:
+            tk.Button(self.buttons_frame, text=text, font=self.buttons_font, command=command ).grid(row=row, column=column,columnspan=columnspan,sticky="snew", padx=3, pady=3)
 
-buttons_font = ("Arial", 15) 
-
-#Attributing weights to the rows and column so the behave dynamic 
-for i in range(4):
-    buttons_frame.grid_columnconfigure(i, weight=1)
-
-for i in range(7):
-    buttons_frame.grid_rowconfigure(i, weight=1)
-
-
-#Functions
-#
-def insert_number_on_display(value):
-    display.configure(state="normal")
-    display_var.set(display_var.get() + str(value))
-    display.configure(state="readonly")
-
-def clean_display():
-    display.configure(state="normal")
-    display_var.set("")
-    display.configure(state='readonly')# Doesn't let the user edit the values directly on the display
-
-def calculate_result():
-    try:
-        result= eval(display_var.get())
-        result = int(result) if isinstance(result, float) and result.is_integer() else result #As the result of eval() always return a float, this code checks if it's integer to format the display value with no d
-        result= str(result)
-        display_var.set(result)
-    
-    except:
-        display.configure(state="normal")
-        display_var.set("Error!")
-        display.configure(state="readonly")
-
-def erase_values():
-    display_values = display_var.get()
-    display_var.set(display_values[:-1]) #Erases the last characterer inserted
-
-
-
-
-def handle_keypress(event):
-    
-    if event.char in "0123456789+-*/":
-        insert_number_on_display(event.char)
-    elif event.keysym=="Return":
-        calculate_result()
-    elif event.keysym=="BackSpace":
-        erase_values()
-
-
-
-
-root.bind("<Key>", handle_keypress)
-
-
-
-
-
-#Creating buttons
-#Buttons of numbers 
-btn_7 = tk.Button(buttons_frame, text="7",font=buttons_font, command=lambda: insert_number_on_display("7"))
-btn_7.grid(row=3, column=0,sticky="nsew", padx=3, pady=3)
-btn_8 = tk.Button(buttons_frame, text="8",font=buttons_font,  command=lambda: insert_number_on_display("8") )
-btn_8.grid(row=3, column=1,sticky="nsew", padx=3, pady=3)
-btn_9 = tk.Button(buttons_frame, text="9",font=buttons_font,  command=lambda: insert_number_on_display("9") )
-btn_9.grid(row=3, column=2,sticky="nsew", padx=3, pady=3)
-
-#Buttons of numbers 
-btn_4 = tk.Button(buttons_frame, text="4",font=buttons_font, command=lambda: insert_number_on_display("4") )
-btn_4.grid(row=4, column=0,sticky="nsew", padx=3, pady=3)
-btn_5 = tk.Button(buttons_frame, text="5",font=buttons_font,  command=lambda: insert_number_on_display("5") )
-btn_5.grid(row=4, column=1,sticky="nsew", padx=3, pady=3)
-btn_6 = tk.Button(buttons_frame, text="6",font=buttons_font, command=lambda: insert_number_on_display("6") )
-btn_6.grid(row=4, column=2,sticky="nsew", padx=3, pady=3)
-
-#Buttons of numbers 
-btn_1 = tk.Button(buttons_frame, text="1",font=buttons_font, command=lambda: insert_number_on_display("1") )
-btn_1.grid(row=5, column=0,sticky="nsew", padx=3, pady=3)
-btn_2 = tk.Button(buttons_frame, text="2",font=buttons_font, command=lambda: insert_number_on_display("2") )
-btn_2.grid(row=5, column=1,sticky="nsew", padx=3, pady=3)
-btn_3 = tk.Button(buttons_frame, text="3",font=buttons_font, command=lambda: insert_number_on_display("3") )
-btn_3.grid(row=5, column=2,sticky="nsew", padx=3, pady=3)
-
-#Buttons of numbers 
-btn_0 = tk.Button(buttons_frame, text="0",font=buttons_font, command=lambda: insert_number_on_display("0") )
-btn_0.grid(row=6, column=0,sticky="nsew", padx=3, pady=3)
-
-#Buttons of signs 
-btn_div = tk.Button(buttons_frame, text="/",font=buttons_font, command=lambda: insert_number_on_display("/"))
-btn_div.grid(row=3, column=3,sticky="nsew", padx=3, pady=3)
-btn_mul = tk.Button(buttons_frame, text="x",font=buttons_font, command=lambda: insert_number_on_display("*") )
-btn_mul.grid(row=4, column=3,sticky="nsew", padx=3, pady=3)
-btn_sub = tk.Button(buttons_frame, text="-",font=buttons_font, command=lambda: insert_number_on_display("-") )
-btn_sub.grid(row=5, column=3,sticky="nsew", padx=3, pady=3)
-btn_add = tk.Button(buttons_frame, text="+",font=buttons_font, command=lambda: insert_number_on_display("+") )
-btn_add.grid(row=6, column=3,sticky="nsew", padx=3, pady=3)
-btn_eq = tk.Button(buttons_frame, text="=",font=buttons_font, command=lambda: calculate_result())
-btn_eq.grid(row=6, column=2,sticky="nsew", padx=3, pady=3)
-btn_dot = tk.Button(buttons_frame, text=".",font=buttons_font, command=lambda: insert_number_on_display(".") )
-btn_dot.grid(row=6, column=1,sticky="nsew", padx=3, pady=3)
-btn_clear = tk.Button(buttons_frame, text="C",font=buttons_font, command=lambda: clean_display())
-btn_clear.grid(row=2, columnspan=4,sticky="nsew", padx=3, pady=3)
+       
 
 
 
 if __name__ == "__main__":
-
- 
-    root.mainloop()
-
+    calculator = CalculatorInterface()
+    calculator.run()

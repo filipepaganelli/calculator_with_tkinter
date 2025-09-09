573a34ac8f83c33dd51cbd2c83aee4d8793f183e
filipepaganelli <paganelli.filipe@gmail.com>
2025-09-09 00:18:37 -0300

    Divides the code into 2 classes: 1 to handle the interface and the other one to handle the engine

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import tkinter as tk
 
 class CalculatorInterface:
     def __init__(self):     
-        #Iniiating the root window and setting attributes 
+        #Iniiating the root window and setting attributes
+        self.engine = CalculatorEngine() 
         self.root = tk.Tk()
         self.root.title("Calculator-Paganelli")
         self.root.geometry("300x400")
@@ -56,8 +57,15 @@ class CalculatorInterface:
         self.root.bind("<Key>", self.handle_keypress)
 
     def handle_keypress(self,event):
-        if event.char in "0123456789+-*/":
+        if event.char in "0123456789":
             self.insert_number_on_display(event.char)
+        elif event.char in "+-*/":
+            last_value = self.engine.expression[-1]
+            if last_value in "+-*/":
+                return 0
+            else:
+                self.insert_number_on_display(event.char)
+
         elif event.keysym=="Return":
             self.calculate_result()
         elif event.keysym=="BackSpace":
@@ -65,35 +73,37 @@ class CalculatorInterface:
 
     def run(self):
         self.root.mainloop()
-
+ 
 
     def insert_number_on_display(self,value):
         self.display.configure(state="normal")
-        self.display_var.set(self.display_var.get() + str(value))
+        self.engine.insert(value)
+        self.display_var.set(self.engine.expression)
         self.display.configure(state="readonly")
 
     def clean_display(self):
         self.display.configure(state="normal")
+        self.engine.clean()
         self.display_var.set("")
         self.display.configure(state='readonly')# Doesn't let the user edit the values directly on the display
 
     def calculate_result(self):
         try:
-            result= eval(self.display_var.get())
-            result = int(result) if isinstance(result, float) and result.is_integer() else result #As the result of eval() always return a float, this code checks if it's integer to format the display value with no d
-            result= str(result)
-            self.display_var.set(result)
-        
-        except:
             self.display.configure(state="normal")
-            self.display_var.set("Error!")
+            result= self.engine.calculate()
+            self.display_var.set(result)
             self.display.configure(state="readonly")
 
+        except:
+            
+            self.display_var.set("Error!")
+            
+
     def erase_values(self):
-        display_values = self.display_var.get()
-        self.display_var.set(display_values[:-1]) #Erases the last characterer inserted
-
-
+        self.display.configure(state="normal")
+        self.engine.clean()
+        self.display_var.set(self.engine.expression) #Erases the last characterer inserted
+        self.display.configure(state="readonly")
 
     def create_buttons(self):
         buttons = [
@@ -121,7 +131,34 @@ class CalculatorInterface:
 
        
 
+class CalculatorEngine:
+    def __init__(self):
+        self.expression= ""
+    
+    def insert(self, value: str):
+        self.expression += str(value)
+    
+    def clean(self):
+        self.expression = self.expression[:-1]
+    
+    def erase(self):
+        self.expression = self.expression[:-1]
+    
+    def calculate(self):
+        try:
+            result= eval(self.expression)
+            if isinstance(result, float) and result.is_integer():
+                result = int(result)
+            else:
+                self.expression = str(result)
 
+            return self.expression
+
+        
+        except Exception:
+            self.expression = ""
+            return "Error"
+    
 
 if __name__ == "__main__":
     calculator = CalculatorInterface()
